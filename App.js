b997ad0eb8f50d64d4410b20df6d26fc32ba8757
iseeghosts <aadarshashutosh@outlook.com>
2020-07-20 14:26:20 +0530

    updated code

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = ({
+      token:0
+    })
+  }
+  update_token = () => {
+    this.setState({
+      token:this.state.token+1
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableHighlight underlayColor='#257' onPress={()=>alert('Plans!')} style={styles.headerbutton}>
+            <Text style={styles.headerbuttontext}>Plans</Text>
+          </TouchableHighlight>
+          <TouchableHighlight underlayColor='#257' onPress={()=>alert('Activity')} style={styles.headerbutton}>
+            <Text style={styles.headerbuttontext}>Activity</Text>
+          </TouchableHighlight>
+          <TouchableHighlight underlayColor='#257' onPress={()=>alert('Track')} style={styles.headerbutton}>
+            <Text style={styles.headerbuttontext}>Track</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    margin:15,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  headerbutton: {
+    backgroundColor:'#fff',
+    elevation:5,
+    paddingHorizontal:10,
+    paddingVertical:5,
+    borderRadius:5,
+    marginHorizontal:10
+  },
+  headerbuttontext: {
+    fontSize:16,
+    color:'darkblue'
+  }
 });
